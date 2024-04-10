75aaf602cbeeebd3eeb21ef3c6f8729741d6c199
justaproger <bolsunros@mail.ru>
2024-04-10 18:25:10 +0300

    DB update.

--- services/dict.py
+++ services/dict.py
@@ -14,6 +14,15 @@ blueprint = Blueprint(
 def dictionary():
     db_sess = db_session.create_session()
     words = db_sess.query(Word).all()
+
+    w = Word(word='ЛИВАТЬ', information='несов. [англ. leave – оставить] – бросать, валить, оставлять, покидать, сваливать, удаляться, уходить.', accent=3)
+    w1 = Word(word='МЕМ', information='мема, м. [англ. meme – мем (единица значимой для культуры информации) <= др. греч. mimema – подобие] – прикол, соль, суть, шутка.', accent=1)
+    w2 = Word(word='ОК/ОКЕЙ', information='[англ. OK – хорошо] – ладно, понятно, хорошо, ясно.', accent=5)
+    db_sess.add(w)
+    db_sess.add(w1)
+    db_sess.add(w2)
+    db_sess.commit()
+
     return render_template('dictionary.html', words=words, title='Dictionary')
 
 
